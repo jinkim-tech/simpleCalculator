9db0f44af45e7edba6fd19e0fe85e40f6ab68552
Jin Kim <jhkim8268@gmail.com>
2020-08-17 22:08:56 -0700

    Add project files.

---
+++ SimpleCalculator.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59c4fd78-41f9-47e9-9bbc-f34822d4a79a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class InputConverter
+    {
+        public double ConvertInputToNumber(string argTextInput)
+        {
+            double convertedNumber;
+
+            if (!double.TryParse(argTextInput, out convertedNumber))
+            {
+                throw new ArgumentException("Expected a numeric value.");
+            }
+            else
+            {
+                return convertedNumber;
+            }
+        }
+    }
+}
